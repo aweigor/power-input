22c5baa8a258992948706a4ab4f2eaae084efdd7
inovitskiy <drmnnml@gmail.com>
2023-06-11 15:39:40 +0300

    initial commit / custom element, selection handler

---
+++ src/main.ts
@@ -0,0 +1,151 @@
+type TEventBusHandler = {
+  id?: string;
+  callback: (...args: any[]) => void;
+}
+
+class EventBus {
+  listeners: Map<string, TEventBusHandler[]> = new Map();
+  subscribe(name: string, handler: TEventBusHandler) {
+    if (!this.listeners.has(name)) {
+      this.listeners.set(name, [handler]);
+    } else {
+      const eventHandlers = this.listeners.get(name);
+      if (!eventHandlers) return;
+      eventHandlers.push(handler);
+    }
+  }
+  unsubscribe(name: string, id: string) {
+    for (const [eventName, eventHandlers] of this.listeners) {
+      for (const [index, handler] of eventHandlers.entries()) {
+        if (handler.id === id && eventName === name) {
+          return eventHandlers.splice(index, 1);
+        }
+      }
+    }
+  }
+  emit(name, ...args: any[]) {
+    const eventHandlers = this.listeners.get(name);
+    if (!eventHandlers) return;
+    eventHandlers.forEach((handler) => {
+      handler.callback(...args);
+    })
+  }
+}
+
+const eventEmitter = new EventBus();
+
+class EventBuffer {}
+
+class EventBufferEntry {
+  element?: HTMLElement;
+  selection?: any;
+}
+
+class EventBufferSelectionEntry extends EventBufferEntry {
+  focusOffset;
+  anchorOffset;
+  focusLine;
+  anchorLine;
+  lines;
+  text;
+  focus;
+  constructor() {
+    super();
+    if (!this.element || !this.selection) return;
+
+    if (!this.isDescendant(this.element, this.selection.focusNode)) {
+      // not implemented yet
+      return
+    }
+    
+    const paragraphs = this.element.innerText.split('\n\n');
+    this.focusOffset = this.selection.focusOffset
+    this.anchorOffset = this.selection.anchorOffset
+    this.focusLine = this.getFocusLine(this.selection)
+    this.anchorLine = this.getAnchorLine(this.selection)
+    this.lines = paragraphs.reduce((acc, el) => el.split('\n').length + acc, 0);
+    this.text = this.element.innerText;
+    this.focus = this.isDescendant(this.element, this.selection.focusNode);
+  }
+  static create(selectionEvent) {
+    return new EventBufferSelectionEntry();
+  }
+  getFocusLine (selection) {
+    let index = this.indexOf(selection.focusNode.parentElement) !== -1 
+      ? this.indexOf(selection.focusNode.parentElement) 
+      : this.indexOf(selection.focusNode);
+    return index + 1;
+  }
+
+  getAnchorLine (selection) {
+    let index = this.indexOf(selection.anchorNode.parentElement) !== -1 
+      ? this.indexOf(selection.anchorNode.parentElement) 
+      : this.indexOf(selection.anchorNode);
+    return index + 1;
+  }
+  indexOf (elt) {
+    if (!this.element) return -1;
+    return Array.from(this.element.children).indexOf(elt);
+  }
+  isDescendant(parent, child) {
+    let node = child;
+
+    while( node ) {
+      if (node == parent) return true;
+      node = node.parentNode;
+    }
+  
+    return false;
+  }
+}
+
+class EventBufferInputEntry extends EventBufferEntry {
+  constructor(event) {
+    super();
+  }
+}
+
+class PowerInput extends HTMLElement {
+  $el?: HTMLDivElement;
+  $events: EventBus;
+  constructor() {
+    super();
+
+    console.log('element created', this)
+    this.$el = this.createInputElement();
+    this.$events = eventEmitter;
+
+    this.attachInputElement();
+  }
+  createInputElement(): HTMLDivElement {
+    const el = document.createElement('div');
+
+    el.setAttribute("contenteditable", "true" );
+    el.setAttribute("tabIndex", "0" );
+
+    return el;
+  }
+  onSelectionChanged(event) {
+    console.log(EventBufferSelectionEntry.create(event));
+  }
+  attachInputElement() {
+    if (!this.$el) return;
+
+    this.appendChild(this.$el);
+    
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.setStart(this.$el, 0);
+    range.setEnd(this.$el, 0 );
+    selection?.addRange(range);
+
+    EventBufferEntry.prototype.element = this.$el;
+    EventBufferEntry.prototype.selection = selection;
+
+    document.addEventListener('selectionchange', this.onSelectionChanged.bind(this))
+
+    this.$events.emit('element-attached', this.$el);
+  }
+}
+
+customElements.define("power-input", PowerInput);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: path.resolve(__dirname, './public/template.html'),
+        filename: 'index.html'
+      }
+    )
+  ]
+}
